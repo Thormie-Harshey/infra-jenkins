9a0718d8a662b591204a221e968bc8c6400f8a27
Thormie Harshey <ashayeadetomiwa77@gmail.com>
2025-06-16 15:14:45 +0100

    updated s3 bucket

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = "~> 1.0"
   backend "s3" {
     bucket = "jenkinseks-infrabucket "
-    key    = "jenkins-iac/terraform.tfstate"
+    key    = "/terraform.tfstate"
     region = "us-east-1"
   }
   required_providers {
